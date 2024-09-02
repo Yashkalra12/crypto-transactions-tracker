b37f9b765127e836d202fbf419b6c941f66e43b8
Yashkalra12 <yash1578.be21@chitkara.edu.in>
2024-09-02 12:03:26 +0530

    Created transactions controller to handle fetch requests

---
+++ src/controller/transactionsController.js
@@ -0,0 +1,24 @@
+const Transaction = require('../models/transactionModel');
+const { getTransactions } = require('../services/etherscanService');
+
+const fetchTransactions = async (req, res) => {
+  try {
+    const { address } = req.params;
+    const transactions = await getTransactions(address);
+
+    const existingTransaction = await Transaction.findOne({ address });
+    if (existingTransaction) {
+      existingTransaction.transactions = transactions;
+      await existingTransaction.save();
+    } else {
+      const newTransaction = new Transaction({ address, transactions });
+      await newTransaction.save();
+    }
+
+    res.status(200).json(transactions);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+module.exports = { fetchTransactions };
